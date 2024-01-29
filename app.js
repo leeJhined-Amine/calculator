0c0921c04e1d69f48f26de864235c7f2786b88fc
leeJhined-Amine <benghezalamine9@gmail.com>
2024-01-29 13:41:30 +0100

    js

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+function showResults(){
+    let input = document.getElementById('output').value;
+
+    let result = eval(input.value);
+    input.value = result;
+}
+
+function calculate(input, operation){
+    let result = input.value 
+}
