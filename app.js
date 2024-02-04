4bf0084ace428b8ae2649a7593503a3a3a903fe2
leeJhined-Amine <benghezalamine9@gmail.com>
2024-02-04 23:18:42 +0100

    Finished

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 function showResults (input){
-    const output = document.getElementById("output");
-    output.value += input;
+    let output = document.getElementById("output");
+    if (output.value == '0'){
+        output.value = input;
+        }
+    else if(input == '+' || input =='-' || input == '*' || input == '.' || input =='/' ){
+            
+            if(output.value[output.value.length-1] != '+' && output.value[output.value.length-1] != '-' && output.value[output.value.length-1] != '*' && output.value[output.value.length-1] != '.' && output.value[output.value.length-1] != '/') {
+                output.value += input;
+            }
+        
+    }
+    else{
+        output.value += input;
+    }
 }
-
 function clearDisplay(){
-    output.value = '';
+    output.value = '0';
 }
 function calculate(){
-    output.value = eval(output.value);
+    try{
+        output.value = eval(output.value);
+    }
+    catch(error){
+        output.value='Error';
+    }
 }
 function Delete(){
     const output = document.getElementById("output");
     output.value = output.value.slice(0,-1);
+    egalClicked = trueK
 }
